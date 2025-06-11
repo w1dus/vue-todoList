e022dac8f1de6e4ee4916344799fbadc1b6c2d97
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-06-11 11:06:25 +0900

    사용자 입력 핸들링

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 
 
 <template>
-  <div>
-    <h1>{{message}}</h1>
-    <input v-model="message" />
-  </div>
+  <h1>{{message}}</h1>
+  <button @click="reserveMessage">reverse Message</button>
+  <!-- <button @click="message += '!'">Append "!"</button> -->
+  <button @click="appendI">Append "!"</button>
+  <a href="https://www.naver.com" @click.prevent="notify">네이버 링크 이동이지만, preventDefault()</a>
 </template>
 
-
+<style>
+  button{ display: block; margin: 0 auto; margin-bottom: 10px;  }
+</style>
 
 <script>
 export default {
   data(){
     return{
-      message : "안녕하세요"
+      message: "Hello World!",
+    }
+  },
+  methods:{
+    appendI(){
+      this.message += '!';
+    },
+    reserveMessage(){
+      // console.log(this.message);
+      this.message = this.message.split('').reverse().join('');
+      // console.log(this.message)
+    },
+    notify(){
+      alert('navigation was prevented.')
+      //e.preventDefault(); 를 하고싶으면 @click.prevent를 꼭 써주자!
     }
   }
 }
